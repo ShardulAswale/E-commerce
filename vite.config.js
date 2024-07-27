c50e384ec34334245d2c52132fdba7364c4ced4f
Shardul Aswale <53704230+ShardulAswale@users.noreply.github.com>
2024-07-27 19:23:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-});
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    server: {
+      watch: {
+        ignored: ["!**/node_modules/autopreview/**"],
+      },
+    },
+    // The watched package must be excluded from optimization,
+    // so that it can appear in the dependency graph and trigger hot reload.
+    optimizeDeps: {
+      exclude: ["autopreview"],
+    },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/E-commerce/'
+  }
+
+  return config
+})
