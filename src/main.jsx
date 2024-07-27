f5d02f13523f2011bc5d96f748062722b14580e2
ShardulAswale <unrealdoc12@gamil.com>
2024-07-27 19:32:09 +0530

    fix: router settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 // src/main.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
+    <HashRouter>
+      <Provider store={store}>
         <App />
-      </Router>
-    </Provider>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>
 );
