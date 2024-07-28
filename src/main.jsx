ebf9f035daff0b6a976a7da88cf2099239b748a3
ShardulAswale <unrealdoc12@gamil.com>
2024-07-28 17:08:52 +0530

    fix: nav 2

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/E-commerce">
       <Provider store={store}>
         <App />
       </Provider>
